d41111117b4ef360ef4501d2ac3fc41adc4740ef
monai <monai@cure.lt>
2016-07-20 15:29:22 +0300

    Cleanup

--- Formula/ccrunner.rb
+++ Formula/ccrunner.rb
@@ -11,8 +11,6 @@ class Ccrunner < Formula
     libexec.install "cc-web-runner-standalone-#{version}.jar"
   end
 
-  plist_options :manual => "ccrunner"
-
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
